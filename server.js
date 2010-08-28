5c0f64ba37abfd6f2613ae34d6fcc106b3d20b95
Kevin Mooney <kmooney@gmail.com>
2010-08-28 17:04:44 -0400

    Massive changes to server for templates, image serving, etc.

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
-var http = require('http');
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var Mu = require ('./lib/mu')
 
+function dump(obj) { 
+	console.log("=====");
+	for (o in obj) { 
+		console.log(o+" "+obj[o]);
+	}
+	console.log("=END=");
+}
+
+Mu.templateRoot = './templates/'
+var context = { serverName: 'Grahnix' } // no context
 var server = http.createServer(function(req,res) { 
-	res.writeHead(200, {"Content-Type": "text/html"})
-	res.end("GRAHNIX because its *BETTER*");
-});
+    // look at the request's path type - if /images/ - we're looking for an image
+	// if /anything_else - we're looking for a template (of that name)
+    var template = 'home.html';
+	if (req.url.indexOf('/images/') == 0) { 
+		// read image and dump
+		fs.readFile(Mu.templateRoot+req.url, function(err,data) { 
+			if (err) throw err
+			res.writeHead(200, {"Content-Type": "image/png"})
+			res.end(data)
+		})
+	}
+	else {  
+		if (req.url != '/') { 
+		template = req.url.substr(1);
+		}
+		res.writeHead(200, {"Content-Type": "text/html"})
+		Mu.render(template, context,{}, function (err, output) { 
+			if (err) { 
+				res.writeHead(404, {"Content-Type": "text/html"}) 
+				res.end("<h1>Page not Found</h1>");
+			}
+			else { 
+				output
+					.addListener('data', function(c) { res.write(c); })
+					.addListener('end',function(){res.end()})
+			}
+		})
+	}
+})
 
-server.listen(80);
+server.listen(80)
